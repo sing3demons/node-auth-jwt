611e48963174894fca8a9cb814dece3479acb7fd
sing3demons <sing3demons@live.com>
2023-08-01 06:59:06 +0700

    edit ci

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-import JsonWebToken from './jwt'
+import jwt from 'jsonwebtoken'
+
+class JsonWebToken {
+  private privateKey: string
+  private publicKey: string
+
+  constructor(privateKey: string, publicKey: string) {
+    this.privateKey = privateKey
+    this.publicKey = publicKey
+  }
+  public signToken(payload: any, expire?: string): string {
+    const token = jwt.sign(payload, this.publicKey, {
+      algorithm: 'RS256',
+      expiresIn: expire ? expire : '1h',
+    })
+    return token
+  }
+
+  public verifyToken(token: string): string | jwt.JwtPayload {
+    const decoded = jwt.verify(token, this.privateKey, { algorithms: ['RS256'] })
+    return decoded
+  }
+}
+
 export default JsonWebToken
