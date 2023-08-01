c686c46db5c21f9701aeb03c25c161ad072db456
sing3demons <sing3demons@live.com>
2023-08-01 09:41:26 +0700

    fix bux

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import jwt from 'jsonwebtoken'
 
 class JsonWebToken {
-  private privateKey: string
-  private publicKey: string
+  private readonly privateKey: string
+  private readonly publicKey: string
 
   constructor(privateKey: string, publicKey: string) {
     this.privateKey = privateKey
     this.publicKey = publicKey
   }
   public signToken(payload: any, expire?: string): string {
-    const token = jwt.sign(payload, this.publicKey, {
+    const token = jwt.sign(payload, this.privateKey, {
       algorithm: 'RS256',
       expiresIn: expire ? expire : '1h',
     })
@@ -17,7 +17,7 @@ class JsonWebToken {
   }
 
   public verifyToken(token: string): string | jwt.JwtPayload {
-    const decoded = jwt.verify(token, this.privateKey, { algorithms: ['RS256'] })
+    const decoded = jwt.verify(token, this.publicKey, { algorithms: ['RS256'] })
     return decoded
   }
 }
